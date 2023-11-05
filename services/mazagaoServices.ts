0754ba95d27e80061729c3724aa8e50eeea906b5
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-11-05 00:07:55 -0300

    DockerFile added

--- services/mazagaoServices.ts
+++ services/mazagaoServices.ts
@@ -3,7 +3,7 @@ import axios, { AxiosError } from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.NEXT_PUBLIC_REACT_APP_API_KEY,
   withCredentials: true,
 });
 
