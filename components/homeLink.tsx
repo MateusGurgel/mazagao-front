6750999f8b4d4a8a223547a0910e909aab6831e0
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-11-15 11:39:37 -0300

    aria-label added on home link

--- components/homeLink.tsx
+++ components/homeLink.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function HomeLink() {
   return (
-    <Link href="/">
+    <Link href="/" aria-label="Vai para página Home">
       <Image src="/icon.png" width={65} height={51} alt="" />
     </Link>
   );
